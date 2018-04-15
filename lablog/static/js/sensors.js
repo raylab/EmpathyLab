3d6711bafc6b5e909f03fe9249ad7d636c48e434
Egor Artemov <egor.artemov@gmail.com>
2018-04-15 19:06:22 +0300

    Draw line chart

--- lablog/static/js/sensors.js
+++ lablog/static/js/sensors.js
@@ -1,213 +1,341 @@
-'use strict'
-
 const RecordButton = {
-  create (isRecording) {
-    const btn = document.createElement('button')
-    this.set(btn, isRecording)
-    return btn
+  create(isRecording) {
+    const btn = document.createElement('button');
+    this.set(btn, isRecording);
+    return btn;
   },
-  set (btn, isRecording) {
-    btn.className = 'js-record-button btn btn-sm ' +
-      (isRecording ? 'btn-danger' : 'btn-secondary')
-    btn.textContent = isRecording ? 'Stop' : 'Record'
-    btn.setAttribute('data-active', isRecording)
+  set(btn, isRecording) {
+    const dom = btn;
+    dom.className = `js-record-button btn btn-sm ${
+      isRecording ? 'btn-danger' : 'btn-secondary'}`;
+    dom.textContent = isRecording ? 'Stop' : 'Record';
+    dom.setAttribute('data-active', isRecording);
   },
-  getAll (container = document.body) {
-    return container.querySelectorAll('.js-record-button')
-  }
+  getAll(container = document.body) {
+    return container.querySelectorAll('.js-record-button');
+  },
+};
+
+function map(n, start1, stop1, start2, stop2) {
+  return (n - start1) / (stop1 - start1) * (stop2 - start2) + start2;
 }
+
+const Chart = {
+  queues: {},
+  header: ['COUNTER', 'INTERPOLATED', 'RAW_CQ', 'AF3', 'F7', 'F3', 'FC5', 'T7', 'P7', 'O1', 'O2', 'P8', 'T8', 'FC6', 'F4', 'F8', 'AF4', 'GYROX', 'GYROY', 'TIMESTAMP', 'MARKER_HARDWARE', 'ES_TIMESTAMP', 'FUNC_ID', 'FUNC_VALUE', 'MARKER', 'SYNC_SIGNAL'],
+  create(sensor) {
+    const chart = document.createElement('canvas');
+    chart.height = 540;
+    chart.width = 600;
+    chart.className = 'js-sensors-chart';
+    this.queues[sensor] = [];
+    return chart;
+  },
+  drawDroppedPackets(chart, frames) {
+    const ctx = chart.getContext('2d');
+    const cnt = frames.length;
+    ctx.strokeStyle = 'rgb(255,0,0)';
+    if (cnt > 1) {
+      for (let i = cnt - 1; i >= 1; i -= 1) {
+        const tNew = frames[i].COUNTER;
+        const tOld = frames[i - 1].COUNTER;
+
+        if (i > 40 && tOld != ((tNew - 1) % 256)) {
+          ctx.beginPath();
+          ctx.moveTo(i, 20);
+          ctx.lineTo(i, chart.height);
+          ctx.stroke();
+        }
+      }
+    }
+  },
+  drawElectrodeData(chart, frames) {
+    const ctx = chart.getContext('2d');
+    const cnt = frames.length;
+    if (cnt > 0) {
+      for (let chan = 3; chan < 16; chan += 1) {
+        const y = map(chan, 3, 15, 70, chart.height - 70);
+        let x = chart.width - 1;
+        ctx.fillStyle = 'rgb(255,255,255)';
+        ctx.fillText(this.header[chan], 5, y);
+        ctx.beginPath();
+        ctx.strokeStyle = 'rgb(255,255,255)';
+        const initial = frames[cnt - 1][this.header[chan]];
+        ctx.moveTo(x, y + (initial - 4200) * 0.5);
+        for (let i = cnt - 1; i >= 0; i -= 1) {
+          const frame = frames[i];
+          const value = frame[this.header[chan]];
+          ctx.lineTo(x--, y + (value - 4200) * 0.5);
+          if (x === 40) { break; }
+        }
+        ctx.stroke();
+      }
+    }
+  },
+  drawTimeGrid(chart, frames) {
+    const ctx = chart.getContext('2d');
+    const cnt = frames.length;
+    ctx.strokeStyle = 'rgb(120,120,120)';
+    ctx.fillStyle = 'rgb(120,120,120)';
+    if (cnt > 1) {
+      for (let i = cnt - 1; i >= 1; i -= 1) {
+        const tNew = frames[i].TIMESTAMP;
+        const tOld = frames[i - 1].TIMESTAMP;
+
+        if (i > 40 && Math.floor(tNew) !== Math.floor(tOld)) {
+          ctx.beginPath();
+          ctx.moveTo(i, 20);
+          ctx.lineTo(i, chart.height);
+          ctx.stroke();
+          ctx.fillText(Math.floor(tNew), i - 8, 16);
+        }
+      }
+    }
+  },
+  draw(chart, frames) {
+    const ctx = chart.getContext('2d');
+    if (chart.width !== chart.clientWidth || chart.height !== chart.clientHeight) {
+      chart.width = chart.clientWidth;
+      chart.height = chart.clientHeight;
+    }
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, chart.width, chart.height);
+    this.drawTimeGrid(chart, frames);
+    this.drawElectrodeData(chart, frames);
+    this.drawDroppedPackets(chart, frames);
+  },
+  getAll(container = document.body) {
+    return container.querySelectorAll('.js-sensors-chart');
+  },
+};
 
 const Headset = {
-  create (sensor, record, isRecording, showRecordButton) {
-    const item = document.createElement('li')
-    item.className = 'js-headset-item list-group-item d-flex justify-content-between align-items-center'
-    item.setAttribute('data-channel', sensor)
-    item.setAttribute('data-record', record)
-    item.appendChild(document.createTextNode(sensor))
+  create(sensor, record, isRecording, showRecordButton) {
+    const item = document.createElement('li');
+    item.className = 'js-headset-item list-group-item d-flex flex-column';
+    item.setAttribute('data-channel', sensor);
+    item.setAttribute('data-record', record);
+    const header = document.createElement('div');
+    header.className = 'd-flex justify-content-between align-items-center';
+    header.appendChild(document.createTextNode(sensor));
     if (showRecordButton) {
-      const btn = RecordButton.create(isRecording)
-      btn.addEventListener('click', function () {
+      const btn = RecordButton.create(isRecording);
+      btn.addEventListener('click', () => {
         if (btn.getAttribute('data-active') === 'true') {
-          WebAPI.stopRecording(sensor, item.getAttribute('data-record'))
+          WebAPI.stopRecording(sensor, item.getAttribute('data-record'));
         } else {
-          item.dispatchEvent(new CustomEvent('startRecording'))
+          item.dispatchEvent(new CustomEvent('startRecording'));
         }
-      })
-      item.appendChild(btn)
+      });
+      header.appendChild(btn);
     }
-    return item
+    item.appendChild(header);
+    item.appendChild(Chart.create(sensor));
+    return item;
   },
 
-  set (item, record, isRecording) {
-    item.setAttribute('data-record', record)
-    const btns = RecordButton.getAll(item)
-    for (let i = 0; i < btns.length; i++) {
-      RecordButton.set(btns[i], isRecording)
+  set(item, record, isRecording) {
+    item.setAttribute('data-record', record);
+    const btns = RecordButton.getAll(item);
+    for (let i = 0; i < btns.length; i += 1) {
+      RecordButton.set(btns[i], isRecording);
     }
   },
 
-  getAll (container = document.body) {
-    return container.querySelectorAll('.js-headset-item')
+  drawData(item, data) {
+    const charts = Chart.getAll(item);
+    for (let i = 0; i < charts.length; i += 1) {
+      const frames = Chart.queues[item.getAttribute('data-channel')];
+      Array.prototype.push.apply(frames, data.frames);
+      if (frames.length > charts[i].width) {
+        frames.splice(0, frames.length - charts[i].width);
+      }
+      window.requestAnimationFrame(() => {
+        Chart.draw(charts[i], frames);
+      });
+    }
   },
 
-  getBySensorAll (container = document.body, sensor) {
-    return container.querySelectorAll('.js-headset-item[data-channel="' + sensor + '"]')
-  }
-}
+  getAll(container = document.body) {
+    return container.querySelectorAll('.js-headset-item');
+  },
+
+  getBySensorAll(container = document.body, sensor) {
+    return container.querySelectorAll(`.js-headset-item[data-channel="${sensor}"]`);
+  },
+};
 
 const HeadsetList = {
-  addHeadset (list, sensor, record, isRecording) {
-    const showRecordButton = list.hasAttribute('data-allow-recording')
-    const item = Headset.create(sensor, record, isRecording, showRecordButton)
-    item.addEventListener('startRecording', function (e) {
-      WebAPI.startRecording(sensor, list.getAttribute('data-experiment'))
-    })
-    list.appendChild(item)
+  addHeadset(list, sensor, record, isRecording) {
+    const showRecordButton = list.hasAttribute('data-allow-recording');
+    const item = Headset.create(sensor, record, isRecording, showRecordButton);
+    item.addEventListener('startRecording', () => {
+      WebAPI.startRecording(sensor, list.getAttribute('data-experiment'));
+    });
+    list.appendChild(item);
   },
 
-  removeHeadset (list, sensor) {
-    const items = Headset.getBySensorAll(list, sensor)
-    for (let i = 0; i < items.length; i++) {
-      items[i].parentNode.removeChild(items[i])
+  removeHeadset(list, sensor) {
+    const items = Headset.getBySensorAll(list, sensor);
+    for (let i = 0; i < items.length; i += 1) {
+      items[i].parentNode.removeChild(items[i]);
     }
   },
 
-  setHeadset (list, sensor, record, isRecording) {
-    const items = Headset.getBySensorAll(list, sensor)
-    for (let i = 0; i < items.length; i++) {
-      Headset.set(items[i], record, isRecording)
+  setHeadset(list, sensor, record, isRecording) {
+    const items = Headset.getBySensorAll(list, sensor);
+    for (let i = 0; i < items.length; i += 1) {
+      Headset.set(items[i], record, isRecording);
     }
   },
 
-  getAll (container = document.body) {
-    return container.querySelectorAll('.headsets-list')
-  }
-}
+  drawData(list, sensor, data) {
+    const items = Headset.getBySensorAll(list, sensor);
+    for (let i = 0; i < items.length; i += 1) {
+      Headset.drawData(items[i], data);
+    }
+  },
+
+  getAll(container = document.body) {
+    return container.querySelectorAll('.headsets-list');
+  },
+};
 
 const RecordItem = {
-  create (record) {
-    const item = document.createElement('tr')
-    item.setAttribute('data-record', record.id)
-    this.set(item, record)
-    return item
+  create(record) {
+    const item = document.createElement('tr');
+    item.setAttribute('data-record', record.id);
+    this.set(item, record);
+    return item;
   },
 
-  set (item, record) {
-    item.innerHTML = '<td>' + record.id + '</td>' +
-      '<td>' + record.StartTime + '</td>' +
-      '<td>' + record.StopTime + '</td>' +
-      '<td>' + record.ObservationMedia1 + '</td>' +
-      '<td>' + record.ObservationMedia2 + '</td>' +
-      '<td>' +
-      '<a href="/lablog/record/' + record.id + '">View</a>, ' +
-      '<a class="text-danger" href="/lablog/record/' + record.id + '/delete/">Delete</a>' +
-      '</td>'
+  set(item, record) {
+    const dom = item;
+    dom.innerHTML = `<td>${record.id}</td>
+      <td>${record.StartTime}</td>
+      <td>${record.StopTime}</td>
+      <td>${record.ObservationMedia1}</td>
+      <td>${record.ObservationMedia2}</td>
+      <td>
+      <a href="/lablog/record/${record.id}">View</a>, 
+      <a class="text-danger" href="/lablog/record/${record.id}/delete/">Delete</a>
+      </td>`;
   },
 
-  getByRecordID (container = document.body, id) {
-    return container.querySelectorAll('tr[data-record="' + id + '"]')
-  }
-}
+  getByRecordID(container = document.body, id) {
+    return container.querySelectorAll(`tr[data-record="${id}"]`);
+  },
+};
 
 const RecordList = {
-  addRecord (list, record) {
-    list.appendChild(RecordItem.create(record))
+  addRecord(list, record) {
+    list.appendChild(RecordItem.create(record));
   },
-  setRecord (list, record) {
-    const items = RecordItem.getByRecordID(list, record.id)
-    for (let i = 0; i < items.length; i++) {
-      RecordItem.set(items[i], record)
+  setRecord(list, record) {
+    const items = RecordItem.getByRecordID(list, record.id);
+    for (let i = 0; i < items.length; i += 1) {
+      RecordItem.set(items[i], record);
     }
   },
-  getByExperimentID (container = document.body, id) {
-    return container.querySelectorAll('.js-records-table[data-experiment="' + id + '"]')
+  getByExperimentID(container = document.body, id) {
+    return container.querySelectorAll(`.js-records-table[data-experiment="${id}"]`);
   },
-  getAll (container = document.body) {
-    return container.querySelectorAll('.js-records-table')
+  getAll(container = document.body) {
+    return container.querySelectorAll('.js-records-table');
+  },
+};
+
+function onAddRecord(data) {
+  const lists = RecordList.getByExperimentID(document.body, data.experiment);
+  for (let i = 0; i < lists.length; i += 1) {
+    RecordList.addRecord(lists[i], data.record);
   }
 }
 
-function onAddRecord (data) {
-  const lists = RecordList.getByExperimentID(document.body, data.experiment)
-  for (let i = 0; i < lists.length; i++) {
-    RecordList.addRecord(lists[i], data.record)
+function onUpdateRecord(data) {
+  const lists = RecordList.getAll();
+  for (let i = 0; i < lists.length; i += 1) {
+    RecordList.setRecord(lists[i], data.record);
   }
 }
 
-function onUpdateRecord (data) {
-  const lists = RecordList.getAll()
-  for (let i = 0; i < lists.length; i++) {
-    RecordList.setRecord(lists[i], data.record)
+function onAddSensor(data) {
+  const lists = HeadsetList.getAll();
+  for (let i = 0; i < lists.length; i += 1) {
+    HeadsetList.addHeadset(lists[i], data.sensor, data.record, data.is_recorded);
   }
 }
 
-function onAddSensor (data) {
-  const lists = HeadsetList.getAll()
-  for (let i = 0; i < lists.length; i++) {
-    HeadsetList.addHeadset(lists[i], data.sensor, data.record, data.is_recorded)
+function onRemoveSensor(data) {
+  const lists = HeadsetList.getAll();
+  for (let i = 0; i < lists.length; i += 1) {
+    HeadsetList.removeHeadset(lists[i], data.sensor);
   }
 }
 
-function onRemoveSensor (data) {
-  const lists = HeadsetList.getAll()
-  for (let i = 0; i < lists.length; i++) {
-    HeadsetList.removeHeadset(lists[i], data.sensor)
+function onUpdateSensor(data) {
+  const lists = HeadsetList.getAll();
+  for (let i = 0; i < lists.length; i += 1) {
+    HeadsetList.setHeadset(lists[i], data.sensor, data.record, data.is_recorded);
   }
 }
 
-function onUpdateSensor (data) {
-  const lists = HeadsetList.getAll()
-  for (let i = 0; i < lists.length; i++) {
-    HeadsetList.setHeadset(lists[i], data.sensor, data.record, data.is_recorded)
+function onRawData(data) {
+  const lists = HeadsetList.getAll();
+  for (let i = 0; i < lists.length; i += 1) {
+    HeadsetList.drawData(lists[i], data.sensor, data.data);
   }
 }
 
 const WebAPI = {
-  init (host) {
-    this.socket = new WebSocket('ws://' + host + '/ws/api')
-    this.socket.onmessage = function (e) {
-      const data = JSON.parse(e.data)
+  init(host) {
+    this.socket = new WebSocket(`ws://${host}/ws/api`);
+    this.socket.onmessage = function parseMessage(e) {
+      const data = JSON.parse(e.data);
       switch (data.command) {
         case 'add_sensor':
-          onAddSensor(data)
-          break
+          onAddSensor(data);
+          break;
         case 'remove_sensor':
-          onRemoveSensor(data)
-          break
+          onRemoveSensor(data);
+          break;
         case 'update_sensor':
-          onUpdateSensor(data)
-          break
+          onUpdateSensor(data);
+          break;
         case 'add_record':
-          onAddRecord(data)
-          break
+          onAddRecord(data);
+          break;
         case 'update_record':
-          onUpdateRecord(data)
-          break
+          onUpdateRecord(data);
+          break;
+        case 'raw_sensor':
+          onRawData(data);
+          break;
+        default:
+          throw new Error('Unknown command');
       }
-    }
+    };
 
-    this.socket.onclose = function (e) {
-      console.error('WebAPI socket closed unexpectedly')
-    }
+    this.socket.onclose = function socketError() {
+      throw new Error('WebAPI socket closed unexpectedly');
+    };
   },
 
-  startRecording (sensor, experiment) {
-    this.socket.send(JSON.stringify(
-      {
-        command: 'start_recording',
-        channel: sensor,
-        experiment: experiment
-      }))
+  startRecording(sensor, experiment) {
+    this.socket.send(JSON.stringify({
+      command: 'start_recording',
+      channel: sensor,
+      experiment,
+    }));
   },
 
-  stopRecording (sensor, record) {
-    this.socket.send(JSON.stringify(
-      {
-        command: 'stop_recording',
-        channel: sensor,
-        record: record
-      }))
-  }
-}
+  stopRecording(sensor, record) {
+    this.socket.send(JSON.stringify({
+      command: 'stop_recording',
+      channel: sensor,
+      record,
+    }));
+  },
+};
 
-WebAPI.init(window.location.host)
+WebAPI.init(window.location.host);
